c11c488adc7e5406c695e2623df06b267e5b62d7
Christopher J. Seeger <cjseeger@iastate.edu>
2020-04-12 15:17:34 -0500

    new r files

---
+++ codeLibrary/R/R/filterCOVID.R
@@ -0,0 +1,88 @@
+install.packages("tidyverse")
+library(tidyverse)
+
+# the rio package will be used to assist with input/output needs. For more info on rio
+#   https://github.com/leeper/rio
+#   https://www.rdocumentation.org/packages/rio/versions/0.5.16
+#   https://www.youtube.com/watch?v=JYVWufSQ4OI
+install.packages("rio")
+library(rio)
+
+
+# Load COVID data
+covidCountyURL_DF <- import("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
+covidCountyURL_DF
+# State COVID data
+covidStateURL_DF <- import("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
+covidStateURL_DF
+
+
+
+
+
+# Filter COVID data, make plot and export data ############################################################
+
+# filtering requires the dplyr or tidyverse package
+
+
+head(covidStateURL_DF)
+
+#select the state of interest
+theState <- "Iowa"
+
+
+mainTitle <- paste("COVID cases in ", theState)
+
+
+#make a dataframe of just North Dakota COVID data by filtering covidStateURL_DF
+filtered_NDcovid_DF <- select(filter(covidStateURL_DF, state == theState), c(date, state, cases, deaths))
+head(filtered_NDcovid_DF) #check the results
+
+# take a look at the field types of filtered_NDcovid_DF and notice the date is a character string, 
+str(filtered_NDcovid_DF)
+# convert date column to date class - see tutorial at 
+# https://www.earthdatascience.org/courses/earth-analytics/time-series-data/date-class-in-r/
+filtered_NDcovid_DF$date <- as.Date(filtered_NDcovid_DF$date, format = "%Y-%m-%d")
+str(filtered_NDcovid_DF) #just confirming the date is now a date
+
+
+days <- NROW(filtered_NDcovid_DF)
+# asOfDate <- filtered_NDcovid_DF$date[days] #line below is better as it formats the date
+asOfDate <- format(as.Date(filtered_NDcovid_DF$date[days]), "%m/%d/%Y")
+totalCases <- filtered_NDcovid_DF$cases[days]
+totalDeaths <- filtered_NDcovid_DF$deaths[days]
+
+# paste("Today is", date())
+
+subtitle <- paste("As of ", asOfDate, " Total cases:", totalCases, " Total deaths:", totalDeaths)
+
+
+ggplot(filtered_NDcovid_DF, aes(x=date, y=cases)) +
+  geom_line() +
+  labs(x = "Date",
+       y = "Cases",
+       title = mainTitle,
+       subtitle = subtitle)
+
+
+# optionally plot as points
+ggplot(data = filtered_NDcovid_DF, aes(x = date, y = cases)) +
+  geom_point() +
+  labs(x = "Deaths",
+       y = "Cases",
+       title = mainTitle,
+       subtitle = subtitle)
+
+
+# optionally include cases and deaths - dashes added as a style
+## reference https://www.datanovia.com/en/blog/how-to-create-a-ggplot-with-multiple-lines/
+ggplot(data = filtered_NDcovid_DF, aes(x = date)) +
+  geom_line(aes(y = cases), color="steelblue", linetype="twodash")  +
+  geom_line(aes(y = deaths), color = "red") +
+  scale_colour_manual("", 
+                      breaks = c("Cases", "Deaths"),
+                      values = c("blue", "red")) +
+  labs(x = "Deaths",
+       y = "Cases",
+       title = mainTitle,
+       subtitle = subtitle)
--- codeLibrary/R/R/rioImport.R
+++ codeLibrary/R/R/rioImport.R
@@ -1,7 +1,7 @@
 install.packages("tidyverse")
 library(tidyverse)
 
-# for more info on rio
+# the rio package will be used to assist with input/output needs. For more info on rio
 #   https://github.com/leeper/rio
 #   https://www.rdocumentation.org/packages/rio/versions/0.5.16
 #   https://www.youtube.com/watch?v=JYVWufSQ4OI
@@ -10,109 +10,64 @@ library(rio)
 
 # Import Data Using Rio #####################################################################################
 
-# import csv file
+# import a csv file
 csvDF <- import("BRSV_GMT.csv")
 csvDF
 
-# import xlsx file
+# import a xlsx file
 xlsxDF <- import("herds.xlsx")
 xlsxDF
 
-# with Excel worksheets, if you don't specify, it selects the first one.
+# When importing an Excel worksheets, if you don't specifythe worksheet it selects the first one.
 xlsDF_months <- import("demoExcelWorksheets.xls")
 xlsDF_months
 
-# worksheets, specify worksheet to import.
+# To specify which Excel worksheet to import.
 xlsDF_favColors <- import("demoExcelWorksheets.xls",  sheet = "favColors")
 xlsDF_favColors
 
-# this will import a list of all the worksheets and their data into var worksheets
+# This will import a list of all the worksheets and their data into the dataframe worksheetDF
 worksheetDF <- import_list("demoExcelWorksheets.xls")
 worksheetDF
-# if this process is used you can then still access the sheet you want using work
+# If this process is used you can still access the exact sheet you want using worksheetDF$ and name of sheet
 head(worksheetDF$months)
 
-# import directly from a web file where the text is already formatted as delimited text
-# in this example, County COVID data from the NYT repository is loaded
+# Import directly from a web file where the text is already formatted as delimited text
+# In these two examples Satante and County COVID data from the New York Times Github repository are loaded
+# A benefit to this methos is that if the data is frequently updated, you just need to rerun the R script and
+# you will be working with the newest data
 covidCountyURL_DF <- import("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
 covidCountyURL_DF
 # State COVID data
 covidStateURL_DF <- import("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
 covidStateURL_DF
 
-# import an html tabel - this all depends on format of HTML web page, but it is a feature of rio
+# Import an html tabel - this all depends on the format of the HTML web page. Complex pages do not seem to work well
 abbrevDF <- import("http://app02.clerk.org/menu/ccis/Help/CCIS%20Codes/state_codes.html")
 abbrevDF
+
+
+# If you need to access a complex HTML table or HTML list, first create a Google Sheet and use the IMPORTHTML()
+# function to pull in the data. Then publish the Google Sheet as a csv file online and use the URL in the import.
+# For more info on IMPORTHTML see https://support.google.com/docs/answer/3093339?hl=en
+# Note Google Sheets also has IMPORTDATA() which will import .csv (comma-separated value) or 
+# .tsv (tab-separated value) formatted data from a specified URL. But rio already does that!
+# This example refers to a Google Sheet that is loading cash grain bids for zip 585785
+# You can see the page here https://www.agweb.com/markets/cash-grain-bids?zip=58575
+# The first table on the page is for Italgrani USA - Benedict, ND - Google Sheets imports jsut this first table
+# but if I wanted Blue Flint Ethonal LLC - Underwood, ND I would tell it to import table 2.
+
+googleSheetsURL <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vSpE_43AzlyPaJ25Gvxp1Tg2oS41n3qfsdUt9MuG3YExMcO43MXLYhN5gdoMtlTiSnhW1HddYCj3MrT/pub?output=csv"
+agwebDF <- import(googleSheetsURL, format = "csv")
+
+myCSV<-read.csv(googleSheetsURL)
 
 
 # Export data #############################################################################
 
 # specify `file` and `format` to override default format of the file export
-export(xlsDF_favColors, file = "mtcars.txt", format = "csv")
+export(xlsDF_favColors, file = "xlsDF_favColors.txt", format = "csv")
 
 # export to JSON
-export(mtcars, "mtcars.json")
+export(xlsDF_favColors, "xlsDF_favColors.json")
 
-
-# Filter COVID data, make plot and export data ############################################################
-
-# filtering requires the dplyr or tidyverse package
-
-
-head(covidStateURL_DF)
-
-#select teh state of interest
-theState <- "North Dakota"
-
-#make a dataframe of just North Dakota COVID data by filtering covidStateURL_DF
-filtered_NDcovid_DF <- select(filter(covidStateURL_DF, state == theState), c(date, state, cases, deaths))
-head(filtered_NDcovid_DF) #check the results
-
-# take a look at the field types of filtered_NDcovid_DF and notice the date is a character string, 
-str(filtered_NDcovid_DF)
-# convert date column to date class - see tutorial at 
-# https://www.earthdatascience.org/courses/earth-analytics/time-series-data/date-class-in-r/
-filtered_NDcovid_DF$date <- as.Date(filtered_NDcovid_DF$date, format = "%Y-%m-%d")
-str(filtered_NDcovid_DF) #just confirming the date is now a date
-
-
-days <- NROW(filtered_NDcovid_DF)
-# asOfDate <- filtered_NDcovid_DF$date[days] #line below is better as it formats the date
-asOfDate <- format(as.Date(filtered_NDcovid_DF$date[days]), "%m/%d/%Y")
-totalCases <- filtered_NDcovid_DF$cases[days]
-totalDeaths <- filtered_NDcovid_DF$deaths[days]
-
-# paste("Today is", date())
-
-subtitle <- paste("As of ", asOfDate, " Total cases:", totalCases, " Total deaths:", totalDeaths)
-
-
-ggplot(filtered_NDcovid_DF, aes(x=date, y=cases)) +
-  geom_line() +
-  labs(x = "Date",
-       y = "Cases",
-       title = "COVID cases in North Dakota",
-       subtitle = subtitle)
-
-
-# optionally plot as points
-ggplot(data = filtered_NDcovid_DF, aes(x = date, y = cases)) +
-  geom_point() +
-  labs(x = "Deaths",
-       y = "Cases",
-       title = "COVID cases in North Dakota",
-       subtitle = subtitle)
-
-
-# optionally include cases and deaths - dashes added as a style
-## reference https://www.datanovia.com/en/blog/how-to-create-a-ggplot-with-multiple-lines/
-ggplot(data = filtered_NDcovid_DF, aes(x = date)) +
-  geom_line(aes(y = cases), color="steelblue", linetype="twodash")  +
-  geom_line(aes(y = deaths), color = "red") +
-  scale_colour_manual("", 
-                      breaks = c("Cases", "Deaths"),
-                      values = c("blue", "red")) +
-  labs(x = "Deaths",
-       y = "Cases",
-       title = "COVID cases in North Dakota",
-       subtitle = subtitle)
