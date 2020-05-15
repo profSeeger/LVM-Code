8539156cb40c2fac402fbe6c987670442519af13
Christopher J. Seeger <cjseeger@iastate.edu>
2020-05-15 11:41:26 -0500

    shinyapp io demo

--- codeLibrary/R/R/rio_tasksheet.R
+++ codeLibrary/R/R/rio_tasksheet.R
@@ -2,3 +2,5 @@ install.packages("rio")
 install_formats() #optional - adds various 'Suggests' dependencies for rio
 library(rio)
 
+# read covid19 from your local
+covid19 <- read.csv("us-counties.csv", header = T)
