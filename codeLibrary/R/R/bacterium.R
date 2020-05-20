2211316a75bd6a39f643cbc7522f6fa4a4ec8c7b
Christopher J. Seeger <cjseeger@iastate.edu>
2020-05-19 23:53:21 -0500

    Update bacterium.R

--- codeLibrary/R/R/bacterium.R
+++ codeLibrary/R/R/bacterium.R
@@ -1,13 +1,15 @@
 # 0 is male, 1 is female
 bacteria <- c(0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1) #bacteria array
 
-newBacteria <- vector()
+newBacteria <- c()
+
+for(i in 1:6) {
 length <- length(bacteria)
 length 
 randPosition = sample(1:length, 1) #select a random number
 randPosition
 
-v1 <- b1[randPosition]
+v1 <- bacteria[randPosition]
 v1 # value of random
 
 bacteria<-bacteria[- randPosition] # will remove value at position p1
@@ -17,7 +19,7 @@ length
 randPosition <- sample(1:length, 1) #select a random number
 randPosition
 
-v2 <- b1[randPosition]
+v2 <- bacteria[randPosition]
 v2 # value of random
 
 bacteria<-bacteria[- randPosition] # will remove value at position p1
@@ -26,5 +28,8 @@ length
 
 append(newBacteria,v1)
 append(newBacteria,v2)
-newBacteria
+newBacteria <- c(v1, v2, i)
+}
+
+
 
