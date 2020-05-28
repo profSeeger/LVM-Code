8071a7d2bed80424e0704f7b129fb3398b5d652d
Christopher J. Seeger <cjseeger@iastate.edu>
2020-05-27 20:58:58 -0500

    Update shinyAppsIO.R

--- codeLibrary/R/R/shinyAppsIO.R
+++ codeLibrary/R/R/shinyAppsIO.R
@@ -7,9 +7,3 @@ rsconnect::deployApp('shinyDemo')
 
 
 #rsconnect::setAccountInfo(name='profseeger', token='FCEFBD5D295162F1338C7E82D8CD284C', secret='m/rVhmI0fjRSpkveKUrN+NI3rMHHJD1vw0FzKnFV')
-
-
-
-
-
-
