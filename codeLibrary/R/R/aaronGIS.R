dcdbaffb3a0807d7cc72e74534e5f813518a767a
Chris Seeger <cjseeger@iastate.edu>
2020-06-26 10:42:17 -0500

    Create aaronGIS.R

---
+++ codeLibrary/R/R/aaronGIS.R
@@ -0,0 +1,14 @@
+
+
+
+# install.packages(c("rnaturalearth", "rnaturalearthdata", "ggspatial", "maps", "tools", "ggrepel", "tmaptools"))
+
+library("ggplot2")
+library("sf")
+library("rnaturalearth")
+library("rnaturalearthdata")
+library("ggspatial")
+library("maps")
+library("tools")
+library("ggrepel")
+library("tmaptools")
