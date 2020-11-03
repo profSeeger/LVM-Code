d3f4008756338a203a7f31cee5e30329054a0106
Chris Seeger <cjseeger@iastate.edu>
2020-11-02 21:28:24 -0600

    read geojson tests

--- codeLibrary/R/R/mapGeojson.R
+++ codeLibrary/R/R/mapGeojson.R
@@ -2,7 +2,31 @@
 install.packages("geojsonio")
 
 library(geojsonio)
-spdf <- geojson_read("https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/communes.geojson",  what = "sp")
+#spdf <- geojson_read("https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/communes.geojson",  what = "sp")
+
+install.packages(c("maps", "mapdata"))
+
+
+library('maps')
+data(us.cities)
+geojson_json(us.cities[1:2, ], lat = 'lat', lon = 'long')
+
+
+myURL <- "https://indicatorstest.extension.iastate.edu/projects/DSPG/database/tinyTests/index.php"
+destfile <- "tinyTests.geojson"
+
+# Apply download.file function in R
+download.file(myURL, destfile)
+
+
+
+
+
+
+
+
+spdf <- geojson_read("https://indicatorstest.extension.iastate.edu/projects/DSPG/database/tinyTests/",  what = "sp")
+
 
 
 # Select only the region #6
@@ -11,9 +35,8 @@ spdf_region_6 = spdf[ spdf@data$mystate == "06" , ]
 spdf_region_7 = spdf[ spdf@data$mystate == "07" , ]
 
 
-# plot the selected are with sp
+# plot the selected area with sp
 library(sp)
 par(mar=c(0,0,0,0))
 plot(spdf_region_6, col="grey")
-
 plot(spdf_region_7, col="red")
